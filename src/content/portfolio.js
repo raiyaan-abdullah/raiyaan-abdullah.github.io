ca7abcbe985bf48665940513992f6907596e0bc6
Raiyaan Abdullah <raiyaanabdullah@gmail.com>
2026-09-16 22:46:57 -0400

    Update portfolio.js

--- src/content/portfolio.js
+++ src/content/portfolio.js
@@ -201,6 +201,7 @@ export const awards = [
   { period: "2026", title: "Spotlight Paper, ECCV 2026" },
   { period: "2026", title: "Best Paper Award, ECCV 2026 GAIA Workshop" },
   { period: "2023–2024", title: "ORCGS Doctoral Fellowship, UCF" },
+  { period: "2022", title: "Graduate Record Examination: 328 (Q: 168, V: 160, AWA: 4.5)" },
   { period: "2020–2021", title: "Special Grant in Research", detail: "Undergraduate thesis project funding from the ICT Division, Government of Bangladesh." },
   { period: "2019–2020", title: "IFIC Bank Trust Fund Research Grant", detail: "Awarded to students in selected departments who excelled in academics and other qualities." },
   { period: "Jan. 2019", title: "2nd - AUST Mindsparks, Ahsanullah University of Science and Technology", detail: "Line Follower Robot Competition" },
